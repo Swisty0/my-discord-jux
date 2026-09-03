a97ecb48a20903888d5d6eb2105dd958ff5ca382
Swisty0 <laxed0481@gmail.com>
2026-09-03 21:52:56 +0300

    Create vip-ss.js

---
+++ commands/vip-ss.js
@@ -0,0 +1,39 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    name: 'vip-ss',
+    description: 'VIP Ss Doğrulama Paneli',
+    async execute(message, args) {
+        if (!message.member.permissions.has(PermissionFlagsBits.Administrator)) {
+            return message.reply('❌ Bu paneli sadece yöneticiler kurabilir!');
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor('#f1c40f')
+            .setTitle('<a:strike:1544076316263972885> 05-SS DOĞRULAMA')
+            .setDescription(
+                '### <a:partimuzik:1544076160445587576> VIP Üyelik Onayı İçin Kanıt Gönderimi\n' +
+                '---\n' +
+                'VIP ayrıcalıklarından yararlanabilmek ve onay almak için ödeme/işlem ekran görüntülerinizi iletmeniz gerekmektedir.\n\n' +
+                '📌 **Nasıl Çalışır?**\n' +
+                '1. Aşağıdaki **`📸 Kanıt Yükle`** butonuna basın.\n' +
+                '2. Sadece sizin ve yetkililerin görebileceği özel bir oda açılacaktır.\n' +
+                '3. Açılan odaya **en az / en fazla 5 adet** ekran görüntüsü yükleyin.\n' +
+                '---\n' +
+                '*Yetkililerimiz fotoğrafları inceleyip VIP rolünüzü en kısa sürede tanımlayacaktır.*'
+            )
+            .setFooter({ text: 'VIP Verification System', iconURL: message.guild.iconURL() })
+            .setTimestamp();
+
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId('vip_ss_talebi_ac')
+                .setLabel('Kanıt Yükle')
+                .setEmoji('<:plus:1544076344927846541>')
+                .setStyle(ButtonStyle.Success)
+        );
+
+        await message.channel.send({ embeds: [embed], components: [row] });
+        message.delete().catch(() => {});
+    }
+};
